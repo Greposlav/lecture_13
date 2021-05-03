09b1b3f7e0a66aa60a3e26ac1f5eec708ef721a9
Greposlav <Tsoltes@seznam.cz>
2021-05-03 10:43:53 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    if n < 0:
+        raise ValueError("Nezadal jsi kladne cislo.")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    m = input("Zadejte cislo:")
+    print(f"{m}-tý prvek Fibonacciho posloupnosti je: {recursive_nth_fibo(int(m))}")
 
 
 if __name__ == '__main__':
